86ecbcb1f6a547fdab45cd7901524eb852fe1a97
Yuri Alexandre Okida Sato <yurimetal4d@gmail.com>
2021-01-22 20:37:42 -0300

    Feedback e colisão prontos.

--- AEDOQUEST/source/script/minigame/mg_keyslaying.gd
+++ AEDOQUEST/source/script/minigame/mg_keyslaying.gd
@@ -3,6 +3,8 @@ extends Node2D
 #Constantes
 const X_AXIS_START = 220 #TODO: Deixar essa constante menos hardcoded...
 const X_AXIS_END = 1240 #TODO: Deixar essa constante menos hardcoded...
+const X_AXIS_ENEMY_PREVIEW = 1032
+const X_AXIS_PLAYER_START = 432
 const EchoShot = preload("res://source/scene/object/dynamic/echoshot.tscn")
 const E = 0
 const F = 1
@@ -13,12 +15,20 @@ const C = 5
 const D = 6
 const E2 = 7
 const F2 = 8
+const testChart = [E, F, G, E, F, G, E, F, G,
+					  A, B, B, A, A, B, F, G, F,
+					  G, G, G, E, E, E, F, F, F, G]
 
 #Variáveis de Estado
 var keyList = []
+var gradPlayerList = []
+var gradEnemyList = []
+var gradFeedbackList = [[],[]]
 var enemyList = [];
 var keyPressed = [];
+var lastLine : String = ""
 var time : float
+var testChartPos : int = 0
 
 #Código Inicial
 func _ready():
@@ -32,27 +42,98 @@ func _ready():
 	['B', $Pentagram/LineB.position.y], 
 	['C', ($Pentagram/LineB.position.y + $Pentagram/LineD.position.y)/2], 
 	['D', $Pentagram/LineD.position.y], 
-	['E2', ($Pentagram/LineD.position.y + $Pentagram/LineF.position.y)/2], 
-	['F2', $Pentagram/LineF.position.y]
+	['E2', ($Pentagram/LineD.position.y + $Pentagram/LineF2.position.y)/2], 
+	['F2', $Pentagram/LineF2.position.y]
 ]
 
-#                 E      F      G      A      B      C      D      E2     F2
-	keyPressed = [false, false, false, false, false, false, false, false, false]
+#                        E      F      G      A      B      C      D      E2     F2
+	keyPressed =       [false, false, false, false, false, false, false, false, false]
+	gradPlayerList =   [false, false, false, false, false, false, false, false, false]
+	gradEnemyList =    [false, false, false, false, false, false, false, false, false]
+	gradFeedbackList = [false, false, false, false, false, false, false, false, false]
 
 #Funções
+func getConstByLine(line : String):
+	if line == 'E':
+		return E
+	elif line == 'F':
+		return F
+	elif line == 'G':
+		return G
+	elif line == 'A':
+		return A
+	elif line == 'B':
+		return B
+	elif line == 'C':
+		return C
+	elif line == 'D':
+		return D
+	elif line == 'E2':
+		return E2
+	elif line == 'F2':
+		return F2
+	else:
+		return null
+
+func getLineByConst(line : int):
+	if line == E:
+		return 'E'
+	elif line == F:
+		return 'F'
+	elif line == G:
+		return 'G'
+	elif line == A:
+		return 'A'
+	elif line == B:
+		return 'B'
+	elif line == C:
+		return 'C'
+	elif line == D:
+		return 'D'
+	elif line == E2:
+		return 'E2'
+	elif line == F2:
+		return 'F2'
+	else:
+		return null
+
 func shoot(line : String, zIndex : int, direction : int):
 	var newEchoShot = EchoShot.instance()
 	add_child(newEchoShot)
 	newEchoShot.setDirection(direction)
+	newEchoShot.line = line
 	if direction == 1:
-		newEchoShot.position.x = X_AXIS_START
+		newEchoShot.position.x = X_AXIS_PLAYER_START
+		gradPlayerList[getConstByLine(line)] = true
 	else:
 		newEchoShot.position.x = X_AXIS_END
+		gradEnemyList[getConstByLine(line)] = true
+	if direction == 1:
+		get_node("Pentagram").get_node("Line" + line).get_node("PlayerGrad").modulate.a = 1.00
+	else:
+		get_node("Pentagram").get_node("Line" + line).get_node("EnemyGrad").modulate.a = 1.00
 	for k in keyList:
 		if k[0] == line:
 			newEchoShot.position.y = k[1]
 			newEchoShot.z_index += zIndex
 			break
+
+func earnCharge(chargePercentage : float):
+	if $Pentagram/Clef/ClefShade.value < 100.0:
+		$Pentagram/Clef/ClefShade.value += chargePercentage
+	else:
+		$Pentagram/Clef/ClefShade.value = 100.0
+
+func takeDamage(damagePercentage : float):
+	if $Pentagram/Clef/Clef.value > 0:
+		$Pentagram/Clef/Clef.value -= damagePercentage
+	else:
+		$Pentagram/Clef/Clef.value = 0.00
+
+func clearShots():
+	for s in get_children():
+		if "EchoShot" in s.name:
+			s.queue_free()
 
 #Código Principal
 func _physics_process(delta):
@@ -62,74 +143,105 @@ func _physics_process(delta):
 	$Background/Sprite.position.x += cos(time)
 	$Background/Sprite.position.y += sin(time)
 	
+	#Apagando gradientes
+	for l in range(gradPlayerList.size()):
+		if gradPlayerList[l]:
+			var PlayerGrad = get_node("Pentagram").get_node("Line" + getLineByConst(l)).get_node("PlayerGrad")
+			if PlayerGrad.modulate.a > 0:
+				PlayerGrad.modulate.a -= 0.025
+			else:
+				PlayerGrad.modulate.a = 0
+				gradPlayerList[l] = false
+	for l in range(gradEnemyList.size()):
+		if gradEnemyList[l]:
+			var EnemyGrad = get_node("Pentagram").get_node("Line" + getLineByConst(l)).get_node("EnemyGrad")
+			if EnemyGrad.modulate.a > 0:
+				EnemyGrad.modulate.a -= 0.025
+			else:
+				EnemyGrad.modulate.a = 0
+				gradEnemyList[l] = false
+	for l in range(gradFeedbackList.size()):
+		if gradFeedbackList[l]:
+			var FeedbackGrad = get_node("Pentagram").get_node("Line" + getLineByConst(l)).get_node("FeedbackGrad")
+			if FeedbackGrad.modulate.a > 0:
+				FeedbackGrad.modulate.a -= 0.025
+			else:
+				FeedbackGrad.modulate.a = 0
+				gradFeedbackList[l] = false
+
 	#Teste de Contramágica
 	if $Timers/TEST_FREQUENCY.is_stopped():
 		$Timers/TEST_FREQUENCY.start()
 	
 	#Input
-	if $KeyslayingGUI/Keys/E.pressed:
+	if $KeyslayingGUI/Keys/E.pressed or Input.is_action_just_pressed("AQ_1"):
 		if !keyPressed[E]:
 			keyPressed[E] = true
 			shoot('E', keyPressed.size() - E, 1)
 	else:
 		keyPressed[E] = false
 
-	if $KeyslayingGUI/Keys/F.pressed:
+	if $KeyslayingGUI/Keys/F.pressed or Input.is_action_just_pressed("AQ_2"):
 		if !keyPressed[F]:
 			keyPressed[F] = true
 			shoot('F', keyPressed.size() - F, 1)
 	else:
 		keyPressed[F] = false
 
-	if $KeyslayingGUI/Keys/G.pressed:
+	if $KeyslayingGUI/Keys/G.pressed or Input.is_action_just_pressed("AQ_3"):
 		if !keyPressed[G]:
 			keyPressed[G] = true
 			shoot('G', keyPressed.size() - G, 1)
 	else:
 		keyPressed[G] = false
 
-	if $KeyslayingGUI/Keys/A.pressed:
+	if $KeyslayingGUI/Keys/A.pressed or Input.is_action_just_pressed("AQ_4"):
 		if !keyPressed[A]:
 			keyPressed[A] = true
 			shoot('A', keyPressed.size() - A, 1)
 	else:
 		keyPressed[A] = false
 
-	if $KeyslayingGUI/Keys/B.pressed:
+	if $KeyslayingGUI/Keys/B.pressed or Input.is_action_just_pressed("AQ_5"):
 		if !keyPressed[B]:
 			keyPressed[B] = true
 			shoot('B', keyPressed.size() - B, 1)
 	else:
 		keyPressed[B] = false
 
-	if $KeyslayingGUI/Keys/C.pressed:
+	if $KeyslayingGUI/Keys/C.pressed or Input.is_action_just_pressed("AQ_6"):
 		if !keyPressed[C]:
 			keyPressed[C] = true
 			shoot('C', keyPressed.size() - C, 1)
 	else:
 		keyPressed[C] = false
 
-	if $KeyslayingGUI/Keys/D.pressed:
+	if $KeyslayingGUI/Keys/D.pressed or Input.is_action_just_pressed("AQ_7"):
 		if !keyPressed[D]:
 			keyPressed[D] = true
 			shoot('D', keyPressed.size() - D, 1)
 	else:
 		keyPressed[D] = false
 
-	if $KeyslayingGUI/Keys/E2.pressed:
+	if $KeyslayingGUI/Keys/E2.pressed or Input.is_action_just_pressed("AQ_8"):
 		if !keyPressed[E2]:
 			keyPressed[E2] = true
 			shoot('E2', keyPressed.size() - E2, 1)
 	else:
 		keyPressed[E2] = false
 
-	if $KeyslayingGUI/Keys/F2.pressed:
+	if $KeyslayingGUI/Keys/F2.pressed or Input.is_action_just_pressed("AQ_9"):
 		if !keyPressed[F2]:
 			keyPressed[F2] = true
 			shoot('F2', keyPressed.size() - F2, 1)
 	else:
 		keyPressed[F2] = false
-	
 
 func _on_TEST_FREQUENCY_timeout():
-	shoot('G', keyPressed.size() - G, -1)
+	#var randLine = getLineByConst(int(rand_range(E, F2)))
+	#shoot(randLine, keyPressed.size() - getConstByLine(randLine), -1)
+	if testChartPos < testChart.size():
+		shoot(getLineByConst(testChart[testChartPos]), keyPressed.size() - testChart[testChartPos], -1)
+	else:
+		testChartPos = -1
+	testChartPos += 1
--- AEDOQUEST/source/script/object/dynamic/echoshot.gd
+++ AEDOQUEST/source/script/object/dynamic/echoshot.gd
@@ -4,6 +4,7 @@ extends Area2D
 const SPEED = 250
 
 #Variáveis de Estado
+var line : String = ""
 var velocity = Vector2()
 var direction = 1
 var damage = 0
@@ -17,11 +18,32 @@ func setDirection(direction : int):
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.position.x *= -1
 		$CollisionShape2D.position.x *= -1
+		$AnimatedSprite.play("fire")
 
 #Código Principal
 func _physics_process(delta):
 	velocity.x = SPEED * direction * delta
 	translate(velocity)
 	
-	if (position.x > 1240 and direction == 1) or (position.x < 220 and direction == -1):
+	#Desinstanciamento por atingir limite do caminho
+	if (position.x > 1240 and direction == 1):
+		queue_free()
+	elif (position.x < 220 and direction == -1):
+		get_parent().takeDamage(5.00)
+		get_parent().clearShots()
+	
+	#Disparo Inimigo
+	if direction == -1 and position.x < 1032 and $AnimatedSprite.animation == "fire":
+		$AnimatedSprite.play("shoot")
+
+func _on_EchoShot_area_entered(area):
+	if "EchoShot" in area.name and area.direction != self.direction and area.direction == -1:
+		get_parent().gradFeedbackList[get_parent().getConstByLine(line)] = true
+		if position.x <= 832 and position.x >= 632:
+			get_parent().get_node("Pentagram").get_node("Line" + line).get_node("FeedbackGrad").modulate.a = 1.00
+			get_parent().earnCharge(2.50)
+		else:
+			get_parent().takeDamage(5.00)
+			get_parent().clearShots()
+		area.queue_free()
 		queue_free()
