6586b1964b5fcea41074c9d415f867a3d1fbdf77
Yuri Alexandre Okida Sato <yurimetal4d@gmail.com>
2021-01-21 21:02:55 -0300

    Contraecho exemplo funcionando.

--- AEDOQUEST/source/script/minigame/mg_keyslaying.gd
+++ AEDOQUEST/source/script/minigame/mg_keyslaying.gd
@@ -18,9 +18,12 @@ const F2 = 8
 var keyList = []
 var enemyList = [];
 var keyPressed = [];
+var time : float
 
 #Código Inicial
 func _ready():
+	time = 0
+	
 	keyList = [
 	['E', $Pentagram/LineE.position.y], 
 	['F', ($Pentagram/LineE.position.y + $Pentagram/LineG.position.y)/2], 
@@ -37,10 +40,14 @@ func _ready():
 	keyPressed = [false, false, false, false, false, false, false, false, false]
 
 #Funções
-func shoot(line : String, zIndex : int):
+func shoot(line : String, zIndex : int, direction : int):
 	var newEchoShot = EchoShot.instance()
 	add_child(newEchoShot)
-	newEchoShot.position.x = X_AXIS_START
+	newEchoShot.setDirection(direction)
+	if direction == 1:
+		newEchoShot.position.x = X_AXIS_START
+	else:
+		newEchoShot.position.x = X_AXIS_END
 	for k in keyList:
 		if k[0] == line:
 			newEchoShot.position.y = k[1]
@@ -49,67 +56,80 @@ func shoot(line : String, zIndex : int):
 
 #Código Principal
 func _physics_process(delta):
+	time += delta
+	
+	#Balanço de Background
+	$Background/Sprite.position.x += cos(time)
+	$Background/Sprite.position.y += sin(time)
+	
+	#Teste de Contramágica
+	if $Timers/TEST_FREQUENCY.is_stopped():
+		$Timers/TEST_FREQUENCY.start()
+	
 	#Input
 	if $KeyslayingGUI/Keys/E.pressed:
 		if !keyPressed[E]:
 			keyPressed[E] = true
-			shoot('E', keyPressed.size() - E)
+			shoot('E', keyPressed.size() - E, 1)
 	else:
 		keyPressed[E] = false
 
 	if $KeyslayingGUI/Keys/F.pressed:
 		if !keyPressed[F]:
 			keyPressed[F] = true
-			shoot('F', keyPressed.size() - F)
+			shoot('F', keyPressed.size() - F, 1)
 	else:
 		keyPressed[F] = false
 
 	if $KeyslayingGUI/Keys/G.pressed:
 		if !keyPressed[G]:
 			keyPressed[G] = true
-			shoot('G', keyPressed.size() - G)
+			shoot('G', keyPressed.size() - G, 1)
 	else:
 		keyPressed[G] = false
 
 	if $KeyslayingGUI/Keys/A.pressed:
 		if !keyPressed[A]:
 			keyPressed[A] = true
-			shoot('A', keyPressed.size() - A)
+			shoot('A', keyPressed.size() - A, 1)
 	else:
 		keyPressed[A] = false
 
 	if $KeyslayingGUI/Keys/B.pressed:
 		if !keyPressed[B]:
 			keyPressed[B] = true
-			shoot('B', keyPressed.size() - B)
+			shoot('B', keyPressed.size() - B, 1)
 	else:
 		keyPressed[B] = false
 
 	if $KeyslayingGUI/Keys/C.pressed:
 		if !keyPressed[C]:
 			keyPressed[C] = true
-			shoot('C', keyPressed.size() - C)
+			shoot('C', keyPressed.size() - C, 1)
 	else:
 		keyPressed[C] = false
 
 	if $KeyslayingGUI/Keys/D.pressed:
 		if !keyPressed[D]:
 			keyPressed[D] = true
-			shoot('D', keyPressed.size() - D)
+			shoot('D', keyPressed.size() - D, 1)
 	else:
 		keyPressed[D] = false
 
 	if $KeyslayingGUI/Keys/E2.pressed:
 		if !keyPressed[E2]:
 			keyPressed[E2] = true
-			shoot('E2', keyPressed.size() - E2)
+			shoot('E2', keyPressed.size() - E2, 1)
 	else:
 		keyPressed[E2] = false
 
 	if $KeyslayingGUI/Keys/F2.pressed:
 		if !keyPressed[F2]:
 			keyPressed[F2] = true
-			shoot('F2', keyPressed.size() - F2)
+			shoot('F2', keyPressed.size() - F2, 1)
 	else:
 		keyPressed[F2] = false
 	
+
+func _on_TEST_FREQUENCY_timeout():
+	shoot('G', keyPressed.size() - G, -1)
--- AEDOQUEST/source/script/object/dynamic/echoshot.gd
+++ AEDOQUEST/source/script/object/dynamic/echoshot.gd
@@ -1,14 +1,27 @@
 extends Area2D
 
 #Constantes
-const SPEED = 500
+const SPEED = 250
 
 #Variáveis de Estado
 var velocity = Vector2()
 var direction = 1
 var damage = 0
 
+#Funções
+func setDirection(direction : int):
+	self.direction = direction
+	if direction == -1:
+		$AnimatedSprite.modulate.g = 0
+		$AnimatedSprite.modulate.r = 0
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.position.x *= -1
+		$CollisionShape2D.position.x *= -1
+
 #Código Principal
 func _physics_process(delta):
 	velocity.x = SPEED * direction * delta
 	translate(velocity)
+	
+	if (position.x > 1240 and direction == 1) or (position.x < 220 and direction == -1):
+		queue_free()
