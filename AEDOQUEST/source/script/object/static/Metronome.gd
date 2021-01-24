05df3c4a1de977aefaee7ecb7d0c6b50be6ca05d
Yuri Alexandre Okida Sato <yurimetal4d@gmail.com>
2021-01-23 21:01:39 -0300

    Ajustes no metronomo.

--- AEDOQUEST/source/script/object/static/Metronome.gd
+++ AEDOQUEST/source/script/object/static/Metronome.gd
@@ -3,19 +3,17 @@ extends Node2D
 #Variáveis de Estado
 var bpm : int
 var time : float
-var side : int
 
 #Código Inicial
 func _ready():
 	bpm = 60
 	$Timer.wait_time = bpm/60.0
 	time = 0
-	side = 0
 
 #Código Principal
 func _physics_process(delta):
 	time += delta
+	$Pointer.rotation_degrees = 60 * cos(PI*time)
 
 func _on_Timer_timeout():
 	$Tempo.play()
-	side == 1
